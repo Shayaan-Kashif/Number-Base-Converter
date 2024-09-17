fd176cff3062c9d908ad19b67b457fe49c14297f
Shayaan Kashif <shayaankashif27@gmail.com>
2024-09-17 18:11:36 -0400

    Implemeted converstion feature to convert from Binary or Hex to Decimal and tested

--- base_converter.py
+++ base_converter.py
@@ -1,3 +1,5 @@
+import math
+
 def ValidateInput(input_number, source_base):
     #Checks base and assigns valid digits depending on said base
     if (source_base == "2"):
@@ -6,7 +8,6 @@ def ValidateInput(input_number, source_base):
     elif (source_base == "10"):
         try:
             float(input_number)
-
 
         except(ValueError):
             return False
@@ -28,7 +29,28 @@ def ValidateInput(input_number, source_base):
     return True
 
 def convert_number(input_number,source_base,target_base):
-    pass #Needs to be coded
+    if(target_base == "2"):
+        pass
+
+    
+    if(target_base == "10"): #If the user wants to convert to decimal 
+        num_list = list(input_number) #Split the user input into a list
+        iteration = len(num_list) - 1 # This is the power the source base will be raised to 
+        hex_replacement = {'A': 10, 'B': 11, 'C': 12,'D':13,'E':14,'F':15} # DIctionary to map hex values
+        result = 0 # Variable to store result
+
+        for num in num_list:
+            if(num in hex_replacement):
+                num = hex_replacement[num] # If the character being iterated over is a hex letter it is mapped to its number value
+
+            result += float(num) * math.pow(int(source_base), iteration) # Converstion from binary to decimal or Hex to decimal
+            iteration -= 1 #Subtracting by one as the code is solving it from left to right
+
+        return result 
+    
+
+    if(target_base == "16"):
+        pass
 
 
 
@@ -51,18 +73,18 @@ while(continue_choice != False):
     if(Validated == True):
         print("\nInput Number Was Validated\n")
         converted_num = convert_number(input_number,source_base,target_base)
-        print(converted_num)
+        print("\nThe result is:",converted_num)
 
     else:
         print("\nThe input",input_number,"was not a valid input for base",source_base+".","Please enter a valid number.\n")
 
 
-    print("Do you wish to contuinue with other numbers? ")
+    print("\nDo you wish to contuinue with other numbers? ")
     print("Enter (Y) to contiue")
     print("Enter (N to quit)")
-    print("Hello")
 
-    wish_to_continue = input("Your Choice: ")
+
+    wish_to_continue = input("\nYour Choice: ")
 
     if(wish_to_continue == "Y"):
         pass #do nothing as the continue_choice variable is already set to true
@@ -71,7 +93,7 @@ while(continue_choice != False):
         continue_choice = False
 
 
-print("Quitting calculator... Thank you!!!")
+print("\nQuitting calculator... Thank you!!!\n")
 
 
 
