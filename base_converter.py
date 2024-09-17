3409e1bbc36fbdd1ba96fea0f9b94c96d7d07f0b
Shayaan Kashif <shayaankashif27@gmail.com>
2024-09-17 19:45:51 -0400

    Implemented the feature to convert Decimal or Hex to Binary and tested it

--- base_converter.py
+++ base_converter.py
@@ -30,13 +30,41 @@ def ValidateInput(input_number, source_base):
 
 def convert_number(input_number,source_base,target_base):
     if(target_base == "2"):
-        pass
+        try:
+            input_number = int(input_number) #Convert to integer and if successful then we continue 
+            result_list = [] #Empty list to store the values of the result but will be backwrds
+            result = "" #Empty string to store the final result in the correct order
 
-    
+            while(input_number != 0): 
+                rem = input_number%2 # Getting the remanider and storing it in rem
+                result_list.append(str(rem)) # Storing the remainder as a string in the result list
+                input_number = math.floor(input_number/2) #Dividing the number by 2 and flooring it to ensure there are no decimals 
+            
+            result_list = result_list[::-1] # Reversing the order of the list
+
+            for num in result_list:
+                result += num #Iterating over the correct order list and storing the result as a single string in result 
+
+            return result #The result is returned 
+
+
+        except(ValueError):
+            hex_to_binary_replacement = {'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101','6':'0110','7':'0111','8':'1000','9':'1001',
+                                         'A': '1010', 'B': '1011', 'C': '1100','D':'1101','E':'1110','F':'1111'} #Dictionary to map the hex values to their corresponding binary values
+            hex_list = list(input_number) # Turning the users input into a list
+            result = "" #Empty string to store the result 
+
+            for hex in hex_list: #iterating over the list 
+                hex = hex_to_binary_replacement[hex] #For each value in the users list it is mapped to its binary value 
+                result += hex # the mapped value is joined into a string
+            
+            return result #The result is returned 
+
+
     if(target_base == "10"): #If the user wants to convert to decimal 
         num_list = list(input_number) #Split the user input into a list
         iteration = len(num_list) - 1 # This is the power the source base will be raised to 
-        hex_replacement = {'A': 10, 'B': 11, 'C': 12,'D':13,'E':14,'F':15} # DIctionary to map hex values
+        hex_replacement = {'A': 10, 'B': 11, 'C': 12,'D':13,'E':14,'F':15} # Dictionary to map hex values
         result = 0 # Variable to store result
 
         for num in num_list:
@@ -46,7 +74,7 @@ def convert_number(input_number,source_base,target_base):
             result += float(num) * math.pow(int(source_base), iteration) # Converstion from binary to decimal or Hex to decimal
             iteration -= 1 #Subtracting by one as the code is solving it from left to right
 
-        return result 
+        return result #The result is returned 
     
 
     if(target_base == "16"):
