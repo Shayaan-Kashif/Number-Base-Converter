42972a833805e25e35d0e3a791e3c88c9c1b6fec
Shayaan Kashif <shayaankashif27@gmail.com>
2024-09-21 14:36:09 -0400

    Implemeted feature to allow negative base 10 numbers to base 16

--- base_converter.py
+++ base_converter.py
@@ -28,73 +28,82 @@ def ValidateInput(input_number, source_base):
             return False
     return True
 
+
+
+def to_binary(input_number, source_base, target_base):
+    try:
+        input_number = float(input_number) #Convert to integer and if successful then we continue 
+        result_list = [] #Empty list to store the values of the result but will be backwrds
+        comp1 = [] #First compliments empty list
+        result = "" #Empty string to store the final result in the correct order
+        bin_replacement = {'1': '0', '0':'1','1.0':'0','0.0':'1'} # Dictionary to change binary values for the 1st compliment
+        negative = False #Variable to check if we have a negative number initialized to false
+        
+
+        if(input_number < 0):
+            negative = True
+            input_number = input_number * -1
+        
+
+        while(input_number != 0): 
+            rem = input_number%2 # Getting the remanider and storing it in rem
+            result_list.append(str(rem)) # Storing the remainder as a string in the result list
+            input_number = math.floor(input_number/2) #Dividing the number by 2 and flooring it to ensure there are no decimals 
+
+
+        result_list = result_list[::-1] # Reversing the order of the list
+
+
+        if(negative == True): #if we have a negative number
+            for num in result_list:
+                num = bin_replacement[num] #Going through each element and swaping the 0 with 1 and 1 wiht 0 for the first compliment 
+                comp1.append(num)
+
+            max_index = len(comp1)-1 #finding the max index number of the list
+
+            while(max_index >= 0): #Startting from the max index which is the element on the far right and checking if it is a 0 if so then we swap to 1n and break the loop else we contiunue till we find one
+
+                if(comp1[max_index]== "0"):
+                    comp1[max_index] = "1"
+                    break
+
+                max_index -= 1 # Decreasing so we can move through the elements from right to left
+
+            
+            result = "1" #negative numbers always start with 1
+            for num in comp1:
+                result += num #Joining into a string 
+
+
+            return result #The result is returned 
+            
+
+        else:
+            for num in result_list:
+                result += num #Iterating over the correct order list and storing the result as a single string in result 
+
+            return result #The result is returned 
+
+
+    except(ValueError):
+        hex_to_binary_replacement = {'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101','6':'0110','7':'0111','8':'1000','9':'1001',
+                                        'A': '1010', 'B': '1011', 'C': '1100','D':'1101','E':'1110','F':'1111'} #Dictionary to map the hex values to their corresponding binary values
+        hex_list = list(input_number) # Turning the users input into a list
+        result = "" #Empty string to store the result 
+
+        for hex in hex_list: #iterating over the list 
+            hex = hex_to_binary_replacement[hex] #For each value in the users list it is mapped to its binary value 
+            result += hex # the mapped value is joined into a string
+        
+        return result #The result is returned 
+    
+    
+
+
 def convert_number(input_number,source_base,target_base):
     if(target_base == "2"):
-        try:
-            input_number = float(input_number) #Convert to integer and if successful then we continue 
-            result_list = [] #Empty list to store the values of the result but will be backwrds
-            comp1 = [] #First compliments empty list
-            result = "" #Empty string to store the final result in the correct order
-            bin_replacement = {'1': '0', '0':'1','1.0':'0','0.0':'1'} # Dictionary to change binary values for the 1st compliment
-            negative = False #Variable to check if we have a negative number initialized to false
-           
-
-            if(input_number < 0):
-                negative = True
-                input_number = input_number * -1
-            
-
-            while(input_number != 0): 
-                rem = input_number%2 # Getting the remanider and storing it in rem
-                result_list.append(str(rem)) # Storing the remainder as a string in the result list
-                input_number = math.floor(input_number/2) #Dividing the number by 2 and flooring it to ensure there are no decimals 
-
-    
-            result_list = result_list[::-1] # Reversing the order of the list
-            print("bin value",result_list)
-
-            if(negative == True): #if we have a negative number
-                for num in result_list:
-                    num = bin_replacement[num] #Going through each element and swaping the 0 with 1 and 1 wiht 0 for the first compliment 
-                    comp1.append(num)
-                print("1st compliment",comp1)
-                max_index = len(comp1)-1 #finding the max index number of the list
-
-                while(max_index >= 0): #Startting from the max index which is the element on the far right and checking if it is a 0 if so then we swap to 1n and break the loop else we contiunue till we find one
-                    print("in loop",max_index)
-                    if(comp1[max_index]== "0"):
-                        comp1[max_index] = "1"
-                        break
-
-                    max_index -= 1 # Decreasing so we can move through the elements from right to left
-                print("second comp",comp1)
-                
-                result = "1" #negative numbers always start with 1
-                for num in comp1:
-                    result += num #Joining into a string 
-
-
-                return result #The result is returned 
-                
-
-            else:
-                for num in result_list:
-                    result += num #Iterating over the correct order list and storing the result as a single string in result 
-
-                return result #The result is returned 
-
-
-        except(ValueError):
-            hex_to_binary_replacement = {'0':'0000','1':'0001','2':'0010','3':'0011','4':'0100','5':'0101','6':'0110','7':'0111','8':'1000','9':'1001',
-                                         'A': '1010', 'B': '1011', 'C': '1100','D':'1101','E':'1110','F':'1111'} #Dictionary to map the hex values to their corresponding binary values
-            hex_list = list(input_number) # Turning the users input into a list
-            result = "" #Empty string to store the result 
-
-            for hex in hex_list: #iterating over the list 
-                hex = hex_to_binary_replacement[hex] #For each value in the users list it is mapped to its binary value 
-                result += hex # the mapped value is joined into a string
-            
-            return result #The result is returned 
+        result = to_binary(input_number,source_base,target_base)
+        return result
 
 
     if(target_base == "10"): #If the user wants to convert to decimal 
@@ -117,14 +126,20 @@ def convert_number(input_number,source_base,target_base):
         input_number_backup = input_number
         input_number = list(input_number)
         length_of_list = len(input_number)
-        num_of_one = input_number.count('1')
-        num_of_zero = input_number.count('0')
+        adjusted_source_base = False
         result = ""
         binary_to_hex_replacement = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7', '1000': '8', '1001': '9', 
                                      '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
         
+        if(input_number[0]=="-"):
+            input_number = input_number_backup
+            input_number = to_binary(input_number, source_base, 2)
+            input_number = list(input_number)
+            length_of_list = len(input_number)
+            adjusted_source_base = True
+        
 
-        if(source_base == "2"):
+        if(source_base == "2" or adjusted_source_base == True):
             #code for binary 
             if(length_of_list % 4== 0):
                 count = 0
@@ -144,19 +159,31 @@ def convert_number(input_number,source_base,target_base):
             elif(length_of_list % 4 == 1):
                 zero_add = 3
                 while(zero_add != 0):
-                    input_number.insert(0,0)
+                    if(adjusted_source_base == True):
+                        input_number.insert(0,1)
+                    else:
+                        input_number.insert(0,0)
+
                     zero_add -= 1
 
             elif(length_of_list % 4 == 2):
                 zero_add = 2
                 while(zero_add != 0):
-                    input_number.insert(0,0)
+                    if(adjusted_source_base == True):
+                        input_number.insert(0,1)
+                        
+                    else:
+                        input_number.insert(0,0)
+                    
                     zero_add -= 1
 
             else:# if the remainder is 3
-                input_number.insert(0,0)
+                if(adjusted_source_base == True):
+                    input_number.insert(0,1)
+                else:
+                    input_number.insert(0,0)
 
-            
+
             count = 0
             placeholder = ""
             for num in input_number: #Loop where the conversion will take place
